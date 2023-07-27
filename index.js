8fdf3cdecffa9c71fac0fcf2acb4b74b6b6a672e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-28 00:27:37 +0100

    updates

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const courseRoutes = require("./routes/courses");
 const port = process.env.PORT || 8080;
+
 app.use(courseRoutes);
+app.use(cors({ origin: "*" }));
+
 
 app.get("/", (req, res) => {
   res.status(200).send({
